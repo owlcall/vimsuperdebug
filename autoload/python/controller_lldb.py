ae4d2154b973e0a121785ae335a4f8d91070d197
Tim Vasko <tvasko@galacticengineering.com>
2017-09-05 20:35:46 -0700

    updated controller to handle state changes adequately; stepping operations block until step is completed, timeouts have been enhanced. Fixed backtrace rendering, navigation.

--- autoload/python/controller_lldb.py
+++ autoload/python/controller_lldb.py
@@ -11,6 +11,7 @@ import lldb
 # Import models which are changed by the controller
 from model_backtrace import Model as Backtrace
 from model_breakpoints import Breakpoint as Breakpoints
+from model_source import Model as Sources
 
 def cerr(data):
 	sys.stderr.write(data)
@@ -42,11 +43,10 @@ def state_type_to_str(enum):
 	elif enum == lldb.eStateSuspended:
 		return "suspended"
 	else:
-		raise Exception("Unknown StateType enum")
+		return "uknown"
 
 class Controller:
 	def __init__(self):
-		print("Setting up LLDB")
 		self.dbg = lldb.SBDebugger.Create()
 		self.commander = self.dbg.GetCommandInterpreter()
 		self.target = None
@@ -55,6 +55,9 @@ class Controller:
 		self.pid = -1
 		self.proc_listener = None
 		self.timeoutEvents = 1		# Number of seconds we wait for events
+		self.timeoutEventsFast = 2	# Events which are expected to return fast end up waiting the longest (to avoid multple calls to refresh)
+
+		self.operation = ""
 
 	def running(self):
 		return self.process != None
@@ -126,39 +129,43 @@ class Controller:
 			cerr("error pausing. No running process.")
 			return
 		self.process.Stop()
-		self.process_events(self.timeoutEvents)
 
 	def resume(self):
 		if not self.process:
 			cerr("error resuming. No running process.")
 			return
 		self.process.Continue()
-		self.process_events(self.timeoutEvents)
 
 	def backtrace(self):
 		if not self.process:
 			cerr("error getting backtrace. No running process.")
 			return
 
+		Backtrace.clear()
 		threadSelected = self.process.GetSelectedThread() 
 		frameSelected = threadSelected.GetSelectedFrame()
 		for _thread in self.process:
-			thread = Backtrace.Thread()
+			thread = Backtrace.thread()
 			thread.default = True if threadSelected == _thread else False
-			thread.number = _thread.GetThreadID()
+			thread.number = _thread.GetIndexID()
 
-			for _frame in thread:
-				frame = Backtrace.Frame()
-				frame.default = True if frameSelected == _frame else False
+			for _frame in _thread:
+				frame = thread.frame()
+				frame.default = True if thread.default and frameSelected.GetFrameID() == _frame.GetFrameID() else False
 				frame.number = _frame.GetFrameID()
 				frame.module = _frame.GetModule().GetFileSpec().GetFilename()
 
 				function = _frame.GetDisplayFunctionName()
-				if function:
+				frame.path = _frame.GetLineEntry().GetFileSpec().GetFilename()
+				if function and _frame.GetLineEntry().GetFileSpec().GetDirectory():
+					# print("detected function")
 					frame.name = _frame.GetFunctionName()
-					frame.path = _frame.GetLineEntry().GetFileSpec().GetFilename()
+					# print("path: "+str(_frame.GetLineEntry().GetFileSpec().GetDirectory()))
+					frame.path = _frame.GetLineEntry().GetFileSpec().GetDirectory()+"/"+_frame.GetLineEntry().GetFileSpec().GetFilename()
 					frame.line = _frame.GetLineEntry().GetLine()
-					frame.column = _frame.GetLineEntry().GetColumn()
+					# frame.column = _frame.GetLineEntry().GetColumn()
+					if not frame.name: frame.name = "<null>"
+					frame.disassembled = False
 				else:
 					# Function is undefined; Load module/assembly
 					# frame.path = frame.module
@@ -166,8 +173,10 @@ class Controller:
 					frame.path = _frame.GetLineEntry().GetFileSpec().GetFilename()
 					#TODO: get address locations for disassembly
 					frame.line = _frame.GetLineEntry().GetLine()
-					frame.column = _frame.GetLineEntry().GetColumn()
-					frame.data = _frame.Disassemble()
+					# frame.column = _frame.GetLineEntry().GetColumn()
+					# frame.data = _frame.Disassemble()
+					frame.disassembled = True
+					if not frame.name: frame.name = "<null2>"
 
 	def breakpoint(self, path, line):
 		if not self.target:
@@ -200,39 +209,90 @@ class Controller:
 			return
 		changed = False
 		selected_thread = self.process.GetSelectedThread()
-		if selected_thread.GetNumber() != frame.thread.number:
-			self.process.SetSelectedThreadByNumber(frame.thread.number)
+		if selected_thread.GetIndexID() != frame.thread.number:
+			self.process.SetSelectedThreadByIndexID(frame.thread.number)
 			selected_thread = self.process.GetSelectedThread()
+			changed = True
 
 		selected_frame = selected_thread.GetSelectedFrame()
-		if selected_frame.GetNumber() != frame.number:
-			self.process.SetSelectedFrame(frame.number)
+		if selected_frame.GetFrameID() != frame.number or changed:
+			selected_thread.SetSelectedFrame(frame.number)
 			changed = True
-		return changed
+
+		Sources.clear()
+		if not frame.disassembled:
+			Sources.path = frame.path
+			Sources.line = frame.line
+		else:
+			Sources.data = frame.data
+			Sources.line = 1
+		# return changed
 
 	def step_over(self):
 		if not self.process:
 			cerr("error stepping over. No running process.")
 			return
 		self.process.GetSelectedThread().StepOver()
-		self.process_events(self.timeoutEvents)
+		state = state_type_to_str(self.process.GetState())
+		if state in ["stepping","stopped","crashed"]:
+			print("initiating stepping...")
+			self.operation = "stepping"
 
 	def step_into(self):
 		if not self.process:
 			cerr("error stepping into. No running process.")
 			return
 		self.process.GetSelectedThread().StepInto()
-		self.process_events(self.timeoutEvents)
+		state = state_type_to_str(self.process.GetState())
+		if state in ["stepping","stopped","crashed"]:
+			print("initiating stepping...")
+			self.operation = "stepping"
 
 	def step_out(self):
 		if not self.process:
 			cerr("error stepping out. No running process.")
 			return
 		self.process.GetSelectedThread().StepOut()
-		self.process_events(self.timeoutEvents)
+		state = state_type_to_str(self.process.GetState())
+		if state in ["stepping","stopped","crashed"]:
+			print("initiating stepping...")
+			self.operation = "stepping"
 
-	def refresh(self):
-		process_events(self)
+	def refresh(self, timeout=0):
+		state = self.process_events(timeout)
+		if state == "invalid":
+			if self.operation == "stepping":
+				state = self.refresh(timeout)
+			pass
+		elif state == "unloaded":
+			print("unloaded")
+		elif state == "connected":
+			print("connected")
+		elif state == "attaching":
+			print("attaching")
+		elif state == "launching":
+			print("launching")
+		elif state == "stopped":
+			self.operation = ""
+			self.backtrace()
+		elif state == "running":
+			if self.operation == "stepping":
+				state = self.refresh(timeout)
+			else:
+				print("running")
+		elif state == "stepping":
+			self.operation = ""
+			self.backtrace()
+		elif state == "crashed":
+			self.operation = ""
+			self.backtrace()
+		elif state == "detached":
+			print("detached")
+		elif state == "exited":
+			print("exited")
+		elif state == "suspended":
+			print("suspended")
+		return state
 
 	def process_events(self, timeout=0):
 		if not self.process:
@@ -240,28 +300,38 @@ class Controller:
 		event = lldb.SBEvent()
 		eventCount = 0
 		state = self.process.GetState()
-		state_new = None
+		state_new = lldb.eStateInvalid
 		done = False
 
 		if state == lldb.eStateInvalid or state == lldb.eStateExited or not self.proc_listener:
+		# if state == lldb.eStateExited or not self.proc_listener:
 			pass
 
+		import time;
+		start = time.time()*1000.0
 		while not done:
+			elapsed = time.time()*1000.0
 			if not self.proc_listener.PeekAtNextEvent(event):
+				if elapsed - start < timeout:
+					time.sleep(0.05)
+				else:
+					done = True
 				# If no events in queue - wait X seconds for events
-				if timeout > 0:
-					self.proc_listener.WaitForEvent(timeout, event)
-					state_new = lldb.SBProcess.GetStateFromEvent(event)
-					eventCount = eventCount + 1
-				done = not self.processListener.PeekAtNextEvent(event)
+				# if timeout > 0:
+					# self.proc_listener.WaitForEvent(timeout, event)
+					# state_new = lldb.SBProcess.GetStateFromEvent(event)
+					# eventCount = eventCount + 1
+				# done = not self.proc_listener.PeekAtNextEvent(event)
 			else:
 				# If events are in queue - process them here
 				self.proc_listener.GetNextEvent(event)
 				state_new = lldb.SBProcess.GetStateFromEvent(event)
 				eventCount = eventCount + 1
-				done = not self.processListener.PeekAtNextEvent(event)
+				if state_new == lldb.eStateInvalid:
+					continue
+				done = not self.proc_listener.PeekAtNextEvent(event)
 
-		#TODO: interact with state_new to see how it compares to state
-		#TODO: this is where the view change could be triggered!
-		pass
+		# print("state changed from %s to %s"%(state_type_to_str(state), state_type_to_str(state_new)))
+		# View changes bubble up from here. First, they're handled on the controller scale in the refresh() function. Then, code bubbles up to plugin.py where the state change is handled on the view's level
+		return state_type_to_str(state_new)
 
